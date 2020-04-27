e7ada4e2240edd78a48eade56e3af13220c356fc
ramon.campos@kukabuzz.onmicrosoft.com <ramon.campos@kukabuzz.com>
2020-04-27 02:47:50 -0300

    Updates

---
+++ precache-manifest.895cf383c2a3b56bbaf5045f818ca836.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f19f0efe56200372fc010cfa8a3b21f",
+    "url": "/viaipi/index.html"
+  },
+  {
+    "revision": "338143620265b546b7a4",
+    "url": "/viaipi/static/js/2.9f82a06f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/viaipi/static/js/2.9f82a06f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b2113035f8be5d66b87",
+    "url": "/viaipi/static/js/main.71519438.chunk.js"
+  },
+  {
+    "revision": "0e55dba341b4a98b9509",
+    "url": "/viaipi/static/js/runtime-main.ecae49ba.js"
+  },
+  {
+    "revision": "1a3da9a154560ce39d6092c16a19f425",
+    "url": "/viaipi/static/media/apple_store.1a3da9a1.svg"
+  },
+  {
+    "revision": "518ab38d76ed1be132a5a19591fb866c",
+    "url": "/viaipi/static/media/google_play.518ab38d.svg"
+  },
+  {
+    "revision": "44d3fce8b9048734c4846f42fa4d229f",
+    "url": "/viaipi/static/media/logo-branca.44d3fce8.svg"
+  },
+  {
+    "revision": "9093b6f082c1da1ee2c5facfe7a0bc5e",
+    "url": "/viaipi/static/media/logo-vertical.9093b6f0.svg"
+  }
+]);
